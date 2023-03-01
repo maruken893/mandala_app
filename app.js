958854c48dd9958f0ef5ea0d7cb94a058f95fd04
maruken893 <kentarooooo555@gmail.com>
2023-03-01 13:50:00 +0900

    Separate app.js and server.js

---
+++ app.js
@@ -0,0 +1,30 @@
+import 'express-async-errors';
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+// router
+import { authRouter } from './routes/index.js';
+
+// middleware
+import { errorHandlerMiddleware } from './middleware/index.js';
+import notFoundMiddleware from './middleware/not-found.js';
+
+const app = express();
+
+// middleware
+app.use(helmet());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// routes
+app.get('/', (req, res) => {
+  res.send('Hello Express!');
+});
+app.use('/api/v1/auth', authRouter);
+
+// app endpoint
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+export default app;
--- server.js
+++ server.js
@@ -1,37 +1,10 @@
-import 'express-async-errors';
-import express from 'express';
-import helmet from 'helmet';
-import morgan from 'morgan';
+import app from './app.js';
 
 // db
 import sequelize from './db/connect.js';
 
-// router
-import { authRouter } from './routes/index.js';
-
-// middleware
-import { errorHandlerMiddleware } from './middleware/index.js';
-import notFoundMiddleware from './middleware/not-found.js';
-
 // setting
 const PORT = process.env.PORT || 8000;
-const app = express();
-
-// middleware
-app.use(helmet());
-app.use(express.json());
-app.use(morgan('dev'));
-
-app.get('/', (req, res) => {
-  res.send('Hello Express!');
-});
-
-// routes
-app.use('/api/v1/auth', authRouter);
-
-// app endpoint
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
 
 const startServer = async () => {
   try {
