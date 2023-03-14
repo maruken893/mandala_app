638e336390727e70b50ddae85581c25042186c4f
maruken893 <kentarooooo555@gmail.com>
2023-03-15 05:58:43 +0900

    Implement changing todo status controller

--- routes/todo/todo.controller.js
+++ routes/todo/todo.controller.js
@@ -24,12 +24,28 @@ export const getTodos = async (req, res) => {
   const PAGE_NUM = 12;
   const page = !req?.query.page ? 0 : req.query.page;
 
-  const todos = await Todo.findAll(
-    {
-      where: { UserId: req.user.uid },
-      limit: PAGE_NUM,
-      offset: PAGE_NUM * page,
-    },
-  );
+  const todos = await Todo.findAll({
+    where: { UserId: req.user.uid },
+    limit: PAGE_NUM,
+    offset: PAGE_NUM * page,
+  });
   res.status(StatusCodes.OK).json({ msg: 'return todos', todos });
+};
+
+export const workOnTodo = async (req, res) => {
+  const { id, toStatusId } = req.body;
+  const todo = await Todo.findOne({
+    where: { id },
+  });
+  if (!todo) {
+    throw new BadRequestError('Todo not found.');
+  }
+  if (todo.UserId !== req.user.uid) {
+    throw new UnauthorizedError("You can't change todo info.");
+  }
+  if (!todo.StatusId === toStatusId) {
+    throw new BadRequestError('Todo status not change');
+  }
+  const updatedTodo = await todo.update({ StatusId: toStatusId });
+  res.status(StatusCodes.OK).json({ msg: 'change', todo: updatedTodo });
 };
--- routes/todo/todo.route.js
+++ routes/todo/todo.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { createTodo, getTodos } from './todo.controller.js';
+import { workOnTodo, createTodo, getTodos } from './todo.controller.js';
 import { auth } from '../../middleware/index.js';
 
 const router = express.Router();
 router.route('/create-todo').post(auth, createTodo);
 router.route('/todos').get(auth, getTodos);
+router.route('/work-on-todo').patch(auth, workOnTodo);
 
 export default router;
