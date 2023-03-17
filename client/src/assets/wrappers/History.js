db671e99a8034b2d10602104c647fa1a99e89bb1
maruken893 <kentarooooo555@gmail.com>
2023-03-18 06:14:25 +0900

    Display Fullcalendar component

---
+++ client/src/assets/wrappers/History.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 20px 30px 20px 30px;
+  border-radius: 5px;
+  box-shadow: 0 0 5px -2px #777777;
+  background-color: white;
+`;
+
+export default Wrapper;
--- client/src/page/dashboard/History.jsx
+++ client/src/page/dashboard/History.jsx
@@ -1,4 +1,20 @@
+import { useState, useEffect } from "react";
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+
+import Wrapper from "../../assets/wrappers/History";
+
 const History = () => {
-  return <div>History</div>;
+  return (
+    <Wrapper>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        events={[]}
+        contentHeight="650px"
+      />
+    </Wrapper>
+  )
 };
 export default History;
