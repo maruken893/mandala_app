ba9c9847ab9bbca6dfaaa69deee64fa85b09f9ba
maruken893 <kentarooooo555@gmail.com>
2023-03-21 23:54:06 +0900

    Add responsive design to mandala chart page

--- client/src/assets/wrappers/MandalaChart.js
+++ client/src/assets/wrappers/MandalaChart.js
@@ -2,103 +2,116 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: 240px 240px 240px;
+  grid-template-columns: 135px 135px 135px;
   justify-content: center;
-  gap: 2%;
-  margin-top: 30px;
-  @media (min-width: 990px) {
+  gap: 3px;
+  margin-top: 20px;
+
+  .inner-container {
+    display: grid;
+    grid-template-columns: 45px 45px 45px;
+  }
+  .cell {
+    background-color: white;
+    height: 60px;
+    padding: 2px;
+    line-height: 1.1;
+    font-size: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .cell:hover {
+    cursor: pointer;
+  }
+  .cell-0 {
+    border-top: 1px solid var(--chart-border-color);
+    border-left: 1px solid var(--chart-border-color);
+    border-top-left-radius: 5px;
+  }
+  .cell-1 {
+    border-top: 1px solid var(--chart-border-color);
+    border-left: 1px solid var(--chart-border-color);
+  }
+  .cell-2 {
+    border-top: 1px solid var(--chart-border-color);
+    border-right: 1px solid var(--chart-border-color);
+    border-left: 1px solid var(--chart-border-color);
+    border-top-right-radius: 5px;
+  }
+  .cell-3 {
+    border-top: 1px solid var(--chart-border-color);
+    border-left: 1px solid var(--chart-border-color);
+  }
+  .cell-4 {
+    border-top: 1px solid var(--chart-border-color);
+    border-left: 1px solid var(--chart-border-color);
+  }
+  .cell-5 {
+    border-top: 1px solid var(--chart-border-color);
+    border-right: 1px solid var(--chart-border-color);
+    border-left: 1px solid var(--chart-border-color);
+  }
+  .cell-6 {
+    border-top: 1px solid var(--chart-border-color);
+    border-left: 1px solid var(--chart-border-color);
+    border-bottom: 1px solid var(--chart-border-color);
+    border-bottom-left-radius: 5px;
+  }
+  .cell-7 {
+    border-top: 1px solid var(--chart-border-color);
+    border-left: 1px solid var(--chart-border-color);
+    border-bottom: 1px solid var(--chart-border-color);
+  }
+  .cell-8 {
+    border: 1px solid var(--chart-border-color);
+    border-bottom-right-radius: 5px;
+  }
+  .goal {
+    font-weight: 500;
+  }
+  .mission {
+    color: #2f2f2f;
+    font-weight: 500;
+  }
+  .mission:hover {
+    opacity: 0.8;
+  }
+  .mission-0 {
+    background-color: #f79797;
+  }
+  .mission-1 {
+    background-color: #f8e688;
+  }
+  .mission-2 {
+    background-color: #dbf888;
+  }
+  .mission-3 {
+    background-color: #fecbff;
+  }
+  .mission-5 {
+    background-color: #88f8b5;
+  }
+  .mission-6 {
+    background-color: #d0baff;
+  }
+  .mission-7 {
+    background-color: #a2cdff;
+  }
+  .mission-8 {
+    background-color: #bdffff;
+  }
+  @media (min-width: 720px) {
+    grid-template-columns: 255px 255px 255px;
+    gap: 7px;
     .inner-container {
-      display: grid;
-      grid-template-columns: 80px 80px 80px;
+      grid-template-columns: 85px 85px 85px;
     }
     .cell {
-      background-color: white;
-      height: 70px;
-      padding: 0.125rem;
+      padding: 4px;
       font-size: 13px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-    .cell:hover {
-      cursor: pointer;
-    }
-    .cell-0 {
-      border-top: 1px solid var(--chart-border-color);
-      border-left: 1px solid var(--chart-border-color);
-      border-top-left-radius: 5px;
-    }
-    .cell-1 {
-      border-top: 1px solid var(--chart-border-color);
-      border-left: 1px solid var(--chart-border-color);
-    }
-    .cell-2 {
-      border-top: 1px solid var(--chart-border-color);
-      border-right: 1px solid var(--chart-border-color);
-      border-left: 1px solid var(--chart-border-color);
-      border-top-right-radius: 5px;
-    }
-    .cell-3 {
-      border-top: 1px solid var(--chart-border-color);
-      border-left: 1px solid var(--chart-border-color);
-    }
-    .cell-4 {
-      border-top: 1px solid var(--chart-border-color);
-      border-left: 1px solid var(--chart-border-color);
-    }
-    .cell-5 {
-      border-top: 1px solid var(--chart-border-color);
-      border-right: 1px solid var(--chart-border-color);
-      border-left: 1px solid var(--chart-border-color);
-    }
-    .cell-6 {
-      border-top: 1px solid var(--chart-border-color);
-      border-left: 1px solid var(--chart-border-color);
-      border-bottom: 1px solid var(--chart-border-color);
-      border-bottom-left-radius: 5px;
-    }
-    .cell-7 {
-      border-top: 1px solid var(--chart-border-color);
-      border-left: 1px solid var(--chart-border-color);
-      border-bottom: 1px solid var(--chart-border-color);
-    }
-    .cell-8 {
-      border: 1px solid var(--chart-border-color);
-      border-bottom-right-radius: 5px;
-    }
-    .goal {
-      font-weight: bold;
-    }
-    .mission {
-      color: #2f2f2f;
-      font-weight: bold;
-    }
-    .mission:hover {
-      opacity: .8;
-    }
-    .mission-0 {
-      background-color: #F79797;
-    }
-    .mission-1 {
-      background-color: #F8E688;
-    }
-    .mission-2 {
-      background-color: #DBF888;
-    }
-    .mission-3 {
-      background-color: #FECBFF;
-    }
-    .mission-5 {
-      background-color: #88F8B5;
-    }
-    .mission-6 {
-      background-color: #D0BAFF;
-    }
-    .mission-7 {
-      background-color: #A2CDFF;
-    }
-    .mission-8 {
-      background-color: #BDFFFF;
+      height: 75px;
+      line-height: 1.25;
     }
   }
 `;
