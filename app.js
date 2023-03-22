8932e5990289a966c4ea4c708e14b017e11250c7
maruken893 <kentarooooo555@gmail.com>
2023-03-22 14:34:16 +0900

    build react app

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import path from 'path';
+
 import 'express-async-errors';
 import express from 'express';
 import helmet from 'helmet';
@@ -17,21 +19,25 @@ import { errorHandlerMiddleware } from './middleware/index.js';
 import notFoundMiddleware from './middleware/not-found.js';
 
 const app = express();
+app.use(express.static(path.join(process.cwd(), 'client', 'build')));
 
 // middleware
 app.use(helmet());
 app.use(express.json());
 app.use(morgan('dev'));
 
+
 // routes
-app.get('/', (req, res) => {
-  res.send('Hello Express!');
-});
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1', goalRouter);
 app.use('/api/v1', missionRouter);
 app.use('/api/v1', subMissionRouter);
 app.use('/api/v1', todoRouter);
+
+// react
+app.get('/*', function (req, res, next) {
+  res.sendFile(path.join(process.cwd(), 'client', 'build', 'index.html'))
+});
 
 // app endpoint
 app.use(notFoundMiddleware);
--- client/src/page/dashboard/TodoList.jsx
+++ client/src/page/dashboard/TodoList.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import { useOutletContext } from 'react-router-dom';
 
 import Wrapper from '../../assets/wrappers/TodoList';
